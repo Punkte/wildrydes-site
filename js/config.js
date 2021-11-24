5166b5ce0bf8e53d61fc55436edcfe40441a51ac
Teepan <paartheepan94@gmail.com>
2021-11-24 14:59:34 +0100

    oui

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-3_PmpOcvN4T', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5oh7kuq3oiv53p7hm5uu3k206h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '1j3to8988i5figs002f1trtccv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-west-3' // e.g. us-east-2
     },
     api: {
